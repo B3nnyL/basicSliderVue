33f82363166b3657986d7b85945104d284ea6b8c
Shuying Lin <shuyinglin47@gmail.com>
2017-09-30 15:58:52 +1000

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,48 @@
 <template>
   <div id="app">
+    <div>
+      <transition name="component-fade" mode="out-in">
+        <component v-bind:is="view[currentCount % view.length]"></component>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  
+  data () {
+    return {
+      view: ['v-a', 'v-b', 'v-c'],
+      timer: null,
+      currentCount: 0
+    }
+  },
+  components: {
+    'v-a': {
+      template: '<img src="https://cdn-images.farfetch-contents.com/12/13/10/56/12131056_10557407_1000.jpg" alt="image1">'
+    },
+    'v-b': {
+      template: '<img src="https://cdn-images.farfetch-contents.com/12/13/10/56/12131056_10557409_1000.jpg" alt="image2">'
+    },
+    'v-c': {
+      template: '<img src="https://cdn-images.farfetch-contents.com/12/13/10/56/12131056_10557410_1000.jpg" alt="image3">'
+    }
+  },
+  mounted: function () {
+    this.startRotation()
+  },
+  methods: {
+    startRotation: function () {
+      this.timer = setInterval(this.next, 3000)
+    },
+    stopRotation: function () {
+      clearInterval(this.timer)
+      this.timer = null
+    },
+    next: function () {
+      this.currentCount += 1
+    }
+  }
 }
 </script>
 
@@ -18,5 +54,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+  }
+
+  .component-fade-enter-active, .component-fade-leave-active {
+    transition: opacity .8s ease;
+  }
+  .component-fade-enter, .component-fade-leave-to
+/* .component-fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
+
+ 
+
 </style>
